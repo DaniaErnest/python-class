37ebef5e5bf10f286c24bc8479f09255e8a82f95
DaniaErnest <daniaernest@aol.com>
2023-04-11 16:40:20 +0100

    slicing

---
+++ 1.Variables/var.py
@@ -0,0 +1,6 @@
+y = 2
+y = "segun"
+x = 3
+d=len(y)
+print("y:",y,"\nx:",x,"\n:",d)
+print(3)
---
+++ 2.indexin,slicing,strings/iss.py
@@ -0,0 +1,8 @@
+mystring = "ABCDEFGHIJ"
+print(mystring[2])
+print(mystring[2:]) #start from 2
+print(mystring[:4]) # don't get to 4
+print(mystring[1:5]) # start from 1, don't get to 5
+print(mystring[::2]) # ACEGI: this mean step size
+print(mystring[2:5:2]) # CE: start from 2, don't get to 5, use step size 2
+print(mystring[::-1])# This reverse a String
