639a921ae87d8b5a4e82544232b689f180fca860
DaniaErnest <daniaernest@aol.com>
2023-04-13 15:19:13 +0100

    Good Job

--- 2.indexin,slicing,strings/iss.py
+++ 2.indexin,slicing,strings/iss.py
@@ -6,3 +6,9 @@ print(mystring[1:5]) # start from 1, don't get to 5
 print(mystring[::2]) # ACEGI: this mean step size
 print(mystring[2:5:2]) # CE: start from 2, don't get to 5, use step size 2
 print(mystring[::-1])# This reverse a String
+
+na = "Tosin"
+delna = na[1:]
+print(delna)
+sln = "f" + delna # concant +
+print(sln)
